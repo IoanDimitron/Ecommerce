952784aa902e0d44775f57ecec69fbd148933461
IoanDimitron <somethinerz1@gmail.com>
2025-02-23 16:45:32 +0200

    Orders

--- ecommerce/app/Models/Order.php
+++ ecommerce/app/Models/Order.php
@@ -3,23 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Model;   
+ use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Order extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'user_id',
-        'grand_total',
-        'payment_method',
-        'payment_status',
-        'status',
-        'currency',
-        'shipping_amount',
-        'shipping_method',
-        'notes',
-    ];
+    protected $fillable = ['user_id', 'payment_method', 'payment_status', 'status', 'currency', 'shipping_method'];
+
+    public function grandTotal(): Attribute
+    {
+        return Attribute::make(
+            get: function () {
+                return $this->items->sum(fn ($item) => $item->total_amount);
+            }
+        );
+    }
+
+
 
     public function user() {
         return $this -> belongsTo(User::class);
