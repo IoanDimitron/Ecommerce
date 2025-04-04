ae196107ad63622d5bc491872684ba6fb45299d4
IoanDimitron <somethinerz1@gmail.com>
2025-04-04 18:32:15 +0300

    Product Detail Page

--- ecommerce/app/Livewire/ProductDetailPage.php
+++ ecommerce/app/Livewire/ProductDetailPage.php
@@ -2,12 +2,21 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
+#[Title('Product Details')]
 class ProductDetailPage extends Component
 {
+    public $slug;
+    public function mount($slug){
+        $this->slug = $slug; //bruzz thks h shkspr 🌹☠ (can't find the emoji)
+    }
     public function render()
     {
-        return view('livewire.product-detail-page');
+        return view('livewire.product-detail-page', [
+            'product' => Product::where('slug', $this->slug)->firstOrFail(),
+        ]);
     }
 }
--- ecommerce/app/Livewire/ProductsPage.php
+++ ecommerce/app/Livewire/ProductsPage.php
@@ -2,12 +2,24 @@
 
 namespace App\Livewire;
 
+use App\Models\Brands;
+use App\Models\Category;
+use App\Models\Product;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+use Livewire\WithPagination;
 
+#[Title('Products')]
 class ProductsPage extends Component
 {
+    use WithPagination;
     public function render()
     {
-        return view('livewire.products-page');
+        $productQuery = Product::query()->where('is_active' , 1);
+        return view('livewire.products-page',[
+            'products' => $productQuery->paginate(6),
+        'brands' => Brands::where('is_active',1)->get(['id','name','slug']),
+        'categories' => Category::where('is_active',1)->get(['id','name','slug']),
+        ]);
     }
 }
